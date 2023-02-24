0ccf4f6b75490497f9e802c09155a198dbd8291f
Abhishek <abhishekparmar162@gmail.com>
2023-02-24 13:40:57 +0530

    added second layer

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,16 +1,27 @@
 import Image from 'next/image'
 import amazon from '../public/assests/amazon.png'
+import {SlLocationPin} from 'react-icons/sl'
+
 
 const NavBar = () => {
   return (
 
-    <div className='bg-[#252F25]'>
+    <div className='bg-[#252F25] text-white flex items-center'>
+        {/* logo */}
         <div>
 <Image width={160} height={160} src={amazon} className='' />
         </div>
-        <div className='bg-[rgb(19,25,33)]'>
+        {/* select your field */}
+    <div className=''>
+        <div>
+        <p>Hello</p>
+        </div>
+        <div className='flex justify-center items-center'>
+            <SlLocationPin size={20} className='text-white'/>
+            <p className='font-semibold'>Select your location</p>
 
         </div>
+    </div>
     </div>
   )
 }
