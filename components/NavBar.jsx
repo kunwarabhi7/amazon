a7fa63c37cd2e988aa48159c14bea61f35643be8
Abhishek <abhishekparmar162@gmail.com>
2023-02-24 14:14:21 +0530

    added search bar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import amazon from '../public/assests/amazon.png'
 import {SlLocationPin} from 'react-icons/sl'
+import SearchBar from './SearchBar'
 
 
 const NavBar = () => {
   return (
 
-    <div className='bg-[#252F25] text-white flex items-center p-1'>
+    <div className='bg-[#252F25] text-white flex items-center p-1 px-1'>
         {/* logo */}
         <div>
 <Image width={160} height={160} src={amazon} className='cursor-pointer hover:border hover:border-white'  />
@@ -22,6 +23,8 @@ const NavBar = () => {
 
         </div>
     </div>
+    {/* Input Bar */}
+    <div><SearchBar /></div>
     </div>
   )
 }
