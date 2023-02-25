ad4a87750bd9b955099f40c8b3e008260d9e9f07
Abhishek <abhishekparmar162@gmail.com>
2023-02-25 11:17:37 +0530

    fix css

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,27 +5,27 @@ import { Carousel } from 'react-responsive-carousel';
 const Hero = () => {
   return (
     <div className="mx-4 bg-[rgb(227,230,230)]">
-         <Carousel autoPlay infiniteLoop showStatus={false}>
+         <Carousel autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false}>
                 <div>
-                    <img src="https://m.media-amazon.com/images/I/71LbHsact9L._SX3000_.jpg" />
+                    <img loading="lazy" src="https://m.media-amazon.com/images/I/71LbHsact9L._SX3000_.jpg" />
                 </div>
                 <div>
-                    <img src="https://m.media-amazon.com/images/I/61GnAucagBL._SX3000_.png" />
+                    <img loading="lazy"  src="https://m.media-amazon.com/images/I/61GnAucagBL._SX3000_.png" />
                 </div>
                 <div>
-                    <img src="https://m.media-amazon.com/images/I/619rl+QqjwL._SX3000_.jpg" />
+                    <img  loading="lazy" src="https://m.media-amazon.com/images/I/619rl+QqjwL._SX3000_.jpg" />
                 </div>
                 <div>
-                    <img src="https://m.media-amazon.com/images/I/51DWgNo1fdL._SX3000_.jpg" />
+                    <img  loading="lazy" src="https://m.media-amazon.com/images/I/51DWgNo1fdL._SX3000_.jpg" />
                 </div>
                 <div>
-                    <img src="https://m.media-amazon.com/images/I/61d4KgLZvZL._SX3000_.jpg" />
+                    <img  loading="lazy" src="https://m.media-amazon.com/images/I/61d4KgLZvZL._SX3000_.jpg" />
                 </div>
                 <div>
-                    <img src="https://m.media-amazon.com/images/I/61mNeTYmm0L._SX3000_.jpg" />
+                    <img  loading="lazy" src="https://m.media-amazon.com/images/I/61mNeTYmm0L._SX3000_.jpg" />
                 </div>
                 <div>
-                    <img src="https://m.media-amazon.com/images/I/71vdTR50hFL._SX3000_.jpg" />
+                    <img loading="lazy"  src="https://m.media-amazon.com/images/I/71vdTR50hFL._SX3000_.jpg" />
                 </div>
             </Carousel>
     </div>
