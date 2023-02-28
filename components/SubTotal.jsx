07c848745a22f87874ccae74cc67d0c5809dc2ff
Abhishek <abhishekparmar162@gmail.com>
2023-02-28 15:04:02 +0530

    added subtotal components

--- components/SubTotal.jsx
+++ components/SubTotal.jsx
@@ -1,16 +1,22 @@
 import { selectItems, selectTotal } from "@/slices/basketSlices";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useSelector } from "react-redux";
+const stripePromise = loadStripe(process.env.stripe_public_key)
+
+
 
 const SubTotal = () => {
     const items =   useSelector(selectItems)
  const total =useSelector(selectTotal)
  const IndianPrize = Math.round(total * 82.93);
 
+ const createCheckOutSession = () => {}
+
   return (
     <div className="m-6 h-auto">
      <h1> Subtotal (${items.length} item): <span className="font-bold text-xl"> ₹ {IndianPrize} </span> </h1>
-      <button className="bg-[rgb(255,216,20)] rounded-full mt-7  w-full px-4 py-3 text-xs sm:text-sm hover:bg-yellow-400 hover:border hover:border-blue-500">
+      <button onClick={createCheckOutSession} role={link} className="bg-[rgb(255,216,20)] rounded-full mt-7  w-full px-4 py-3 text-xs sm:text-sm hover:bg-yellow-400 hover:border hover:border-blue-500">
         Proceed to Buy
       </button>
     </div>
