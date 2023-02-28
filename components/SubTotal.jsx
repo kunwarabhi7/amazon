d3d5e220d77e2a1153a8fd4c3a18f8b61c58c744
Abhishek <abhishekparmar162@gmail.com>
2023-02-28 13:29:14 +0530

    added subtotal components

--- components/SubTotal.jsx
+++ components/SubTotal.jsx
@@ -1,9 +1,15 @@
+import { selectItems, selectTotal } from "@/slices/basketSlices";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const SubTotal = () => {
+    const items =   useSelector(selectItems)
+ const total =useSelector(selectTotal)
+ const IndianPrize = Math.round(total * 82.93);
+
   return (
     <div className="m-6 h-auto">
-     <h1> Subtotal (1 item): <span className="font-bold text-xl"> ₹ 599.00 </span> </h1>
+     <h1> Subtotal (${items.length} item): <span className="font-bold text-xl"> ₹ {IndianPrize} </span> </h1>
       <button className="bg-[rgb(255,216,20)] rounded-full mt-7  w-full px-4 py-3 text-xs sm:text-sm hover:bg-yellow-400 hover:border hover:border-blue-500">
         Proceed to Buy
       </button>
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -3,6 +3,7 @@ import FullCart from "@/components/FullCart";
 import LowerNav from "@/components/LowerNav";
 import NavBar from "@/components/NavBar";
 import SubTotal from "@/components/SubTotal";
+
 import { useSession } from "next-auth/react";
 import React from "react";
 
